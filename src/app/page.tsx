c28ce4c6ca05823dda8a87c2546773f2dbd01143
Amar Prasad <amarprasad77354@gmail.com>
2024-12-10 08:47:10 +0530

    auth-completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+"use client"
+// import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main>
+    <main className="min-h-screen bg-slate-700">
         <h1 className="">Home page</h1>
+
+        <Button onClick={()  => router.push("/jobs")} variant="default" className="bg-slate-500 border-rose-600">
+            <span  className="">Explore jobs</span>                
+        </Button>
     </main>
   );
 }
--- src/layouts/appbar.tsx
+++ src/layouts/appbar.tsx
@@ -3,11 +3,17 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Appbar() {
-    const session = useSession();
+    const {data: session, status} = useSession();
     const router = useRouter();
 
+    console.log("Data-", session, status);
+
+    if (status === "loading") {
+        return <div className="flex p-[14px] justify-around bg-slate-500">Loading...</div>; // Show loading state while the session is being refetched
+    }
     return (
         <main>
             <div className="flex p-2 justify-around bg-slate-500">
@@ -16,18 +22,24 @@ export default function Appbar() {
                         <Link href="/" className="">Content Society</Link>
                     </div>
                 </div>
-                <div className="variable-menu">
-                    <Button onClick={() => router.push("/signin")} variant="outline" className="bg-inherit border-black">
+                <div className="variable-menu flex gap-[10px] items-center">
+                    <Button onClick={()  => router.push("/signin")} variant="outline" className="bg-inherit border-black">
                         <span  className="">Login</span>                
                     </Button>
-                    <Button variant="outline" className="bg-inherit border-black">
-                        <Link href="/" className="" onClick={() => {
-                            signOut({redirect:false});
-                            router.refresh()
-                        }}>Logout</Link>                
+                    <Button variant="outline" className="bg-inherit border-black"
+                        onClick={() => {
+                            signOut({ 
+                                callbackUrl: "/",
+                                redirect: true, 
+                             });
+                            router.refresh();
+                        }}
+                    >
+                        Logout                
                     </Button>
+                    <div className="cursor-pointer"><Image className="rounded-full" src={session?.user?.image || '' } width={30} height={30} alt="profile-image" /></div>
                 </div>
-                {JSON.stringify(session)}
+                {/* {JSON.stringify(session)} */}
             </div>
         </main>
     )
