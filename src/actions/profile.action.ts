fefc134f29a883ef7317c8e01d9c773ad80e4811
amarprasadpati <amarprasad.pati@inovaare.com>
2025-02-11 15:01:32 +0530

    profile-creation  flow

--- src/actions/profile.action.ts
+++ src/actions/profile.action.ts
@@ -4,6 +4,36 @@ import { ProfileSchemaType } from "@/lib/schema/profileSchema";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
+export async function updateProfile(data: ProfileSchemaType, imageUrl: string | undefined) {
+    try {
+        const session = await getServerSession(authOptions);
+        console.log("session", session);
+        if (!session?.user?.email) {
+            return { status: false, error: "You are not authorized, please log in." };
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: {
+                email: session.user.email
+            },
+            data: {
+                profileImg: imageUrl,
+                portfolioUrl: data.portfolioUrl,
+                skills: data.skills,
+                experience: data.experience,
+                role: 'APPLICANT'
+            }
+        });
+
+        return { status: true, user: updatedUser };
+    } catch (error) {
+        console.error("Error updating profile:", error);
+        return { status: false, error: "Failed to update profile" };
+    }
+}
+
+
+// To get the user profile
 export async function getUserProfile() {
     try {
         const session = await getServerSession(authOptions);
@@ -28,32 +58,5 @@ export async function getUserProfile() {
     } catch (error) {
         console.error("Error fetching user profile:", error);
         return { status: false, error: "Failed to fetch profile" };
-    }
-}
-
-export async function updateProfile(data: ProfileSchemaType) {
-    try {
-        const session = await getServerSession(authOptions);
-        if (!session?.user?.email) {
-            return { status: false, error: "Unauthorized" };
-        }
-
-        const updatedUser = await prisma.user.update({
-            where: {
-                email: session.user.email
-            },
-            data: {
-                profileImg: data.profileImg,
-                portfolioUrl: data.portfolioUrl,
-                skills: data.skills,
-                experience: data.experience,
-                role: data.role
-            }
-        });
-
-        return { status: true, user: updatedUser };
-    } catch (error) {
-        console.error("Error updating profile:", error);
-        return { status: false, error: "Failed to update profile" };
     }
 }
