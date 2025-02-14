6302a5f8d91b470eea8247f3888e0139a2ba3fd1
Amar Prasad <amarprasad77354@gmail.com>
2025-02-15 00:39:15 +0530

    profiles page err fix

---
+++ src/app/(employer-perpective)/profiles/ProfilesLayout.tsx
@@ -0,0 +1,16 @@
+"use server"
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
+
+export default async function ProfilesLayout({ children }: { children: React.ReactNode }) {
+    const session = await getServerSession(authOptions);
+
+    if (!session || session?.user?.role !== "EMPLOYER") {
+        // show a toast saying that "you have to create job to view the profiles"
+        // redirect("/signin?callbackUrl=/profiles");
+        redirect("/?callbackUrl=/profiles");
+    }
+
+    return <>{children}</>;
+}
--- src/app/(employer-perpective)/profiles/page.tsx
+++ src/app/(employer-perpective)/profiles/page.tsx
@@ -1,90 +1,13 @@
-"use client"
-import { getProfiles } from "@/actions/profile.action";
-import prisma from "@/config/prisma.config";
-import { useToast } from "@/hooks/use-toast";
-import { UserRole } from "@prisma/client";
-import { useSession } from "next-auth/react";
-import { redirect, usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 
-interface Profile {
-    id: string;
-    role: UserRole;
-    name: string;
-    profileImg: string | null;
-    portfolioUrl: string | null;
-    skills: string[];
-    experience: string | null;
-    email: string;
-}
+import ProfilesPage from "@/components/profiles";
+import ProfilesLayout from "./ProfilesLayout";
+
+
 
 export default function Profiles() {
-    const {data: session, status} = useSession();
-    const { toast } = useToast();
-    const pathName = usePathname();
-    const [initialReq, setInitialReq] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const [profiles, setProfiles] = useState<Profile[]>([]);
-
-    useEffect(() => {
-        console.log("profiles", profiles);
-    }, [profiles])
-
-    useEffect(() => {
-        console.log("SESSION frm--profiles ->", session)
-        // if(session?.user?.role !== "EMPLOYER"){
-        //   console.log("NOT an Employer")
-        //   toast({
-        //     variant: 'destructive',
-        //     title: "Access Denied",
-        //     description: "You have create a job to view the profiles.",
-        //   })
-        // //   return redirect("/");
-        // }
-        async function fetchProfiles() {            
-            try {
-                setIsLoading(true);
-                console.log("INSIDE FETCH PROFILES")
-                const result = await getProfiles();
-                if (result.status && result.profiles) {
-                    setProfiles(result.profiles);
-                } else {
-                    toast({
-                        variant: 'destructive',
-                        title: "Error fetching profiles",
-                        description: "Please try again later.",
-                    });
-                }
-            } catch (error) {
-                console.error("Error fetching profiles:", error);
-                toast({
-                    variant: 'destructive',
-                    title: "Error fetching profiles",
-                    description: "Please try again later.",
-                });
-            }
-        }
-        
-        fetchProfiles();
-
-    }, [toast]);
-
-    if (status === "loading") {
-        return <div>Loading...</div>
-    }
-    if (status === "unauthenticated") {
-        toast({
-          variant: 'destructive',
-          title: "You are not signed in! Please sign in to see the profiles.",
-        });
-        // return redirect("/signin");
-        return redirect(`/signin?callbackUrl=${encodeURIComponent(pathName)}`);
-    }
-
     return (
-        <div className="container w-full min-h-screen flex justify-center py-7">
-            <h2 className="text-white">Find the best talent for your needs</h2>
-
-        </div>
+        <ProfilesLayout>
+            <ProfilesPage />
+        </ProfilesLayout>
     )
 }
