e04c00718df997d5ba0d80952cd8d559083c91e1
Amar Prasad <amarprasad77354@gmail.com>
2025-02-23 23:44:57 +0530

    type-er

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -7,14 +7,15 @@ const ERROR_NAME = {
     AUTHENTICATION_FAILED: 'Authentication failed',
     USER_NOT_FOUND: 'User not found',
     NOT_FOUND: 'Not Found'
-}
+} as const;
 const STATUS_CODES = {
     UNAUTHORIZED: 401,
     BAD_REQUEST: 400,
     INTERNAL_SERVER_ERROR: 501,
     AUTHENTICATION_FAILED: 404,
+    USER_NOT_FOUND: 404,
     NOT_FOUND: 404
-}
+} as const;
 
 export type ErrorResponseType = {
     name: string;
