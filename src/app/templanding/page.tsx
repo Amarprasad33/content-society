39b25033c390ae88437b7202ad33b405154acea0
amar <amarp4578@gmail.com>
2025-03-30 23:04:21 +0530

    changes in card-1-b3nto

--- src/app/templanding/page.tsx
+++ src/app/templanding/page.tsx
@@ -163,17 +163,25 @@ export default function TempLanding() {
             {/* Card-1 */}
             <div className="relative w-[640px] h-[420px] rounded-2xl bg-[linear-gradient(to_top,_rgba(18,14,37,0.84)_14%,_rgba(53,35,144,0.69)_69%)]">
               {/* Search Filter cards border-[#282D43] */}
-
-              <div className="relative rounded-lg p-4 w-80 border border-[#282D43]">
-                {/* Gradient Background */}
-                <div className="absolute inset-0 bg-[#101129] opacity-80 rounded-lg"></div>
-                <div className="absolute inset-0 bg-[linear-gradient(to_bottom_left,_rgba(120,86,220,0.4)_-47%,_rgba(0,0,0,0.1)_70%)] rounded-lg"></div>
-                
-                {/* Solid Color Overlay */}
-                  
-                <div className="relative z-10 text-white">
-                  <div className="text-lg font-medium">Categories</div>
-                  <div className="text-sm text-gray-300">Creative · Post production · Design</div>
+              <div className="cards-container absolute left-32 top-8 border border-slate-300">
+                <div className="flex gap-3">
+                  <div className="relative rounded-lg p-4 w-80 border border-[#373d59]">
+                    {/* Gradient Background */}
+                    <div className="absolute inset-0 bg-[#101129] opacity-80 rounded-lg"></div>
+                    <div className="absolute inset-0 bg-[linear-gradient(to_bottom_left,_rgba(120,86,220,0.4)_-47%,_rgba(0,0,0,0.1)_70%)] rounded-lg"></div>
+                    
+                    {/* Solid Color Overlay */}
+                      
+                    <div className="relative z-10 text-white flex flex-col gap-3">
+                      <div className="text-sm font-medium">Categories</div>
+                      <div className="text-[12px] font-normal flex gap-2 text-white/60">
+                        <div>Creative</div>
+                        <div>Post production</div>
+                        <div>Design</div>
+                        <div>Video editing</div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
               {/* Copy */}
--- src/components/custom/heroProfileCard.tsx
+++ src/components/custom/heroProfileCard.tsx
@@ -8,146 +8,146 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 
-export default function HeroProfileCard(){
+export default function HeroProfileCard() {
     return (
         <Card className="h-fit bg-[#110d27] text-white rounded-3xl relative overflow-hidden border border-green-500">
             <CardContent className="py-8 px-8">
 
-            <div className="flex items-center gap-4">
-                <Avatar className="w-[70px] h-[70px]">
-                <AvatarImage
-                    src='/templanding/ellipse-4.png'
-                    alt='Ava Scott'
-                />
-                {/* Should be the first char. of the name */}
-                <AvatarFallback>
-                    A
-                </AvatarFallback>
-                </Avatar>
+                <div className="flex items-center gap-4">
+                    <Avatar className="w-[70px] h-[70px]">
+                        <AvatarImage
+                            src='/templanding/ellipse-4.png'
+                            alt='Ava Scott'
+                        />
+                        {/* Should be the first char. of the name */}
+                        <AvatarFallback>
+                            A
+                        </AvatarFallback>
+                    </Avatar>
 
-                <div className="flex-1">
-                <div className="flex items-center gap-2">
-                    <h3 className="[font-family:'Inter',Helvetica] font-medium text-white text-[18px]">
-                    Ava Scott
-                    </h3>
-                    <div className="relative w-[18px] h-[18px]">
-                    <Image
-                        src={"/templanding/vefication.svg"}
-                        alt="Verification"
-                        className="absolute w-full h-full"
-                        width={18}
-                        height={18}
-                    />
+                    <div className="flex-1">
+                        <div className="flex items-center gap-2">
+                            <h3 className="[font-family:'Inter',Helvetica] font-medium text-white text-[18px]">
+                                Ava Scott
+                            </h3>
+                            <div className="relative w-[18px] h-[18px]">
+                                <Image
+                                    src={"/templanding/vefication.svg"}
+                                    alt="Verification"
+                                    className="absolute w-full h-full"
+                                    width={18}
+                                    height={18}
+                                />
+                            </div>
+                        </div>
+                        <p className="[font-family:'Inter',Helvetica] font-medium text-[#6b6b6b] text-[18px]">
+                            Script writer
+                        </p>
+                    </div>
+
+                    <button className="w-[34px] h-[34px] flex items-center justify-center">
+                        <img
+                            src='/templanding/material-symbols-bookmark-outline-rounded.svg'
+                            alt="Bookmark"
+                            className="w-full h-full"
+                        />
+                    </button>
+                </div>
+
+                <div className="pt-8 space-y-4">
+                    <div className="flex flex-wrap gap-2">
+                        <Badge
+                            variant="outline"
+                            className="h-7 bg-[#7171712e] rounded-[25px] border-[#aeaeae] shadow-[inset_4px_7px_13.5px_#0000006b] px-3 py-1"
+                        >
+                            <img
+                                src={'/templanding/bytesize-work.svg'}
+                                alt="Work type"
+                                className="w-5 h-5 mr-2"
+                            />
+                            <span className="[font-family:'Inter',Helvetica] font-normal text-white text-sm">
+                                Full-time
+                            </span>
+                        </Badge>
+
+                        <Badge
+                            variant="outline"
+                            className="h-7 bg-[#7171712e] rounded-[25px] border-[#aeaeae] shadow-[inset_4px_7px_13.5px_#0000006b] px-3 py-1"
+                        >
+                            <div className="w-4 h-4 mr-2">
+                                <img
+                                    src={'/templanding/group-4.png'}
+                                    alt="Experience"
+                                    className="w-full h-full"
+                                />
+                            </div>
+                            <span className="[font-family:'Inter',Helvetica] font-normal text-white text-sm">
+                                3+ Years
+                            </span>
+                        </Badge>
+                    </div>
+
+                    <div className="flex flex-wrap gap-2">
+                        <Badge
+                            variant="outline"
+                            className="h-7 bg-[#7171712e] rounded-[25px] border-[#aeaeae] shadow-[inset_4px_7px_13.5px_#0000006b] px-3 py-1"
+                        >
+                            <img
+                                src={'/templanding/tdesign-money.svg'}
+                                alt="Rate"
+                                className="w-5 h-5 mr-2"
+                            />
+                            <span className="[font-family:'Inter',Helvetica] font-normal text-white text-sm">
+                                $60/hr
+                            </span>
+                            <div className="w-4 h-4 mx-1 flex items-center justify-center">
+                                <div className="w-[7px] h-[7px] bg-white rounded-[3.5px]"></div>
+                            </div>
+                            <span className="[font-family:'Inter',Helvetica] font-normal text-white text-sm">
+                                Negotiable
+                            </span>
+                        </Badge>
+
+                        <Badge
+                            variant="outline"
+                            className="h-7 bg-[#7171712e] rounded-[25px] border-[#aeaeae] shadow-[inset_4px_7px_13.5px_#0000006b] px-3 py-1"
+                        >
+                            <img
+                                src={'/templanding/weui-location-outlined-1.svg'}
+                                alt="Location"
+                                className="w-5 h-5 mr-2"
+                            />
+                            <span className="[font-family:'Inter',Helvetica] font-normal text-white text-sm">
+                                Onsite: CA, LA
+                            </span>
+                        </Badge>
                     </div>
                 </div>
-                <p className="[font-family:'Inter',Helvetica] font-medium text-[#6b6b6b] text-[18px]">
-                    Script writer
-                </p>
+
+                {/* Separator */}
+                <div className="py-8">
+                    <div className="w-full h-[0.1rem] bg-white/30" />
                 </div>
 
-                <button className="w-[34px] h-[34px] flex items-center justify-center">
-                <img
-                    src='/templanding/material-symbols-bookmark-outline-rounded.svg'
-                    alt="Bookmark"
-                    className="w-full h-full"
-                />
-                </button>
-            </div>
+                {/* Actino Button */}
+                <div className="flex flex-col items-center gap-4">
+                    <Button className="w-[200px] h-10 bg-[#2d23634f] rounded-[58px] text-white hover:bg-[#2d2363] relative overflow-hidden">
+                        <div className="absolute w-[145px] h-[38px] top-[-40px] left-[1.7rem] bg-[#7533c7] rounded-[103px/19px] blur-[18.15px]"></div>
+                        <div className="absolute w-[145px] h-[38px] bottom-[-36px] left-[1.7rem] bg-[#7533c7] rounded-[103px/19px] blur-[18.15px]"></div>
+                        <span className="relative z-10 [font-family:'Inter',Helvetica] font-normal text-sm">
+                            Hire
+                        </span>
+                    </Button>
 
-            <div className="pt-8 space-y-4">
-                <div className="flex flex-wrap gap-2">
-                <Badge
-                    variant="outline"
-                    className="h-7 bg-[#7171712e] rounded-[25px] border-[#aeaeae] shadow-[inset_4px_7px_13.5px_#0000006b] px-3 py-1"
-                >
-                    <img
-                    src={'/templanding/bytesize-work.svg'}
-                    alt="Work type"
-                    className="w-5 h-5 mr-2"
-                    />
-                    <span className="[font-family:'Inter',Helvetica] font-normal text-white text-sm">
-                    Full-time
-                    </span>
-                </Badge>
-
-                <Badge
-                    variant="outline"
-                    className="h-7 bg-[#7171712e] rounded-[25px] border-[#aeaeae] shadow-[inset_4px_7px_13.5px_#0000006b] px-3 py-1"
-                >
-                    <div className="w-4 h-4 mr-2">
-                    <img
-                        src={'/templanding/group-4.png'}
-                        alt="Experience"
-                        className="w-full h-full"
-                    />
-                    </div>
-                    <span className="[font-family:'Inter',Helvetica] font-normal text-white text-sm">
-                    3+ Years
-                    </span>
-                </Badge>
+                    <Button
+                        variant="ghost"
+                        className="w-[200px] h-10 rounded-[58px] [background:linear-gradient(90deg,rgba(255,255,255,1)_0%,rgba(153,153,153,1)_100%)] [-webkit-background-clip:text] bg-clip-text hover:bg-transparent"
+                    >
+                        <span className="[font-family:'Inter',Helvetica] font-normal text-sm">
+                            Discuss
+                        </span>
+                    </Button>
                 </div>
-
-                <div className="flex flex-wrap gap-2">
-                <Badge
-                    variant="outline"
-                    className="h-7 bg-[#7171712e] rounded-[25px] border-[#aeaeae] shadow-[inset_4px_7px_13.5px_#0000006b] px-3 py-1"
-                >
-                    <img
-                    src={'/templanding/tdesign-money.svg'}
-                    alt="Rate"
-                    className="w-5 h-5 mr-2"
-                    />
-                    <span className="[font-family:'Inter',Helvetica] font-normal text-white text-sm">
-                    $60/hr
-                    </span>
-                    <div className="w-4 h-4 mx-1 flex items-center justify-center">
-                    <div className="w-[7px] h-[7px] bg-white rounded-[3.5px]"></div>
-                    </div>
-                    <span className="[font-family:'Inter',Helvetica] font-normal text-white text-sm">
-                    Negotiable
-                    </span>
-                </Badge>
-
-                <Badge
-                    variant="outline"
-                    className="h-7 bg-[#7171712e] rounded-[25px] border-[#aeaeae] shadow-[inset_4px_7px_13.5px_#0000006b] px-3 py-1"
-                >
-                    <img
-                    src={'/templanding/weui-location-outlined-1.svg'}
-                    alt="Location"
-                    className="w-5 h-5 mr-2"
-                    />
-                    <span className="[font-family:'Inter',Helvetica] font-normal text-white text-sm">
-                    Onsite: CA, LA
-                    </span>
-                </Badge>
-                </div>
-            </div>
-
-            {/* Separator */}
-            <div className="py-8">
-                <div className="w-full h-[0.1rem] bg-white/30" />
-            </div>
-
-            {/* Actino Button */}
-            <div className="flex flex-col items-center gap-4">
-                <Button className="w-[200px] h-10 bg-[#2d23634f] rounded-[58px] text-white hover:bg-[#2d2363] relative overflow-hidden">
-                <div className="absolute w-[145px] h-[38px] top-[-40px] left-[1.7rem] bg-[#7533c7] rounded-[103px/19px] blur-[18.15px]"></div>
-                <div className="absolute w-[145px] h-[38px] bottom-[-36px] left-[1.7rem] bg-[#7533c7] rounded-[103px/19px] blur-[18.15px]"></div>
-                <span className="relative z-10 [font-family:'Inter',Helvetica] font-normal text-sm">
-                    Hire
-                </span>
-                </Button>
-
-                <Button
-                variant="ghost"
-                className="w-[200px] h-10 rounded-[58px] [background:linear-gradient(90deg,rgba(255,255,255,1)_0%,rgba(153,153,153,1)_100%)] [-webkit-background-clip:text] bg-clip-text hover:bg-transparent"
-                >
-                <span className="[font-family:'Inter',Helvetica] font-normal text-sm">
-                    Discuss
-                </span>
-                </Button>
-            </div>
             </CardContent>
         </Card>
     )
