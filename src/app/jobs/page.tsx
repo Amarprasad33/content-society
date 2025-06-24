935540d909811968ff64b9b59b0e5f465463d714
amarprasadpati <amarprasad.pati@inovaare.com>
2025-06-24 16:24:24 +0530

    added loading screens and fixed job-view ui

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -8,7 +8,7 @@ import { useEffect, useRef, useState } from "react"
 import { AnimatePresence, motion } from "motion/react"
 import { JobSchemaType } from "@/lib/schema/jobSchema"
 
-interface JobType extends JobSchemaType{
+interface JobType extends JobSchemaType {
   id: string,
   orgLogo: string,
   Salary?: string,
@@ -18,13 +18,14 @@ interface JobType extends JobSchemaType{
 export default function Jobs() {
   const [jobs, setJobs] = useState<JobType[]>([]);
   const [jobDetailView, setJobDetailView] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [selectedJobId, setSelectedJobId] = useState('');
   const viewRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if(jobDetailView){
+    if (jobDetailView) {
       document.body.style.overflow = 'hidden';
-    }else{
+    } else {
       document.body.style.overflow = 'auto';
     }
 
@@ -33,9 +34,11 @@ export default function Jobs() {
     }
   }, [jobDetailView])
 
-  
+
 
   useEffect(() => {
+    setLoading(true);
+    console.log('loading', loading)
     const fetchJobs = async () => {
       const jobsData = await getJobs();
       const processedJobs = jobsData.map(job => ({
@@ -47,13 +50,15 @@ export default function Jobs() {
         experience: job.experience?.toString() || undefined,
       }));
       console.log("jobs", processedJobs);
-       // Convert `null` orgLogo values to an empty string
+      // Convert `null` orgLogo values to an empty string
       setJobs(processedJobs);
+      setLoading(false);
+      console.log('loading', loading)
     }
     fetchJobs();
 
     const handleMouseClick = (event: MouseEvent) => {
-      if(viewRef.current && !viewRef.current.contains(event.target as Node)){
+      if (viewRef.current && !viewRef.current.contains(event.target as Node)) {
         setJobDetailView(false);
       }
     }
@@ -64,18 +69,31 @@ export default function Jobs() {
     }
   }, []);
 
-  
-
+  if (loading) {
+    return (
+      <div className="jobs-page flex flex-col items-center justify-center h-screen ">
+        <div role="status">
+          <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div className="text-3xl mt-8 text-white font-semibold">Loading....</div>
+      </div>
+    )
+  }
 
   return (
-    <div className="jobs-page border flex justify-center border-green-600 mt-20 ">
+    <div className="jobs-page flex flex-col items-center justify-center mt-20 ">
+      <h1 className="font-semibold text-4xl mb-8">Explore Jobs</h1>
       <div className="max-w-6xl flex flex-col gap-4">
         {jobs.map((job: JobType) => (
           <Card onClick={() => {
-              setJobDetailView(true);
-              setSelectedJobId(job.id)
-            }} 
-            key={job.id} 
+            setJobDetailView(true);
+            setSelectedJobId(job.id)
+          }}
+            key={job.id}
             className="w-full max-w-2xl md:min-w-[40rem] bg-inherit hover:bg-[#111111] border-zinc-800 p-4"
           >
             <div className="flex items-start justify-between">
@@ -96,7 +114,7 @@ export default function Jobs() {
                     </span>
                   </div>
                   <p className="text-zinc-400 text-sm mt-0.5">
-                    <CustomTooltip triggerContent={job.description?? ''} tooltipContent={job.description?? ''} className="bg-white text-black shadow-md" />
+                    <CustomTooltip triggerContent={job.description ?? ''} tooltipContent={job.description ?? ''} className="bg-white text-black shadow-md" />
                   </p>
                   <p className="text-zinc-500 text-sm mt-0.5">Posted 3 days ago</p>
                 </div>
@@ -119,8 +137,8 @@ export default function Jobs() {
               </span>
               <span className="px-2.5 py-1 bg-zinc-800 text-emerald-400 rounded-full text-xs">
                 {/* $ {(job.minSalary)/1000}k-{(job.maxSalary)/1000}k */}
-                {job.Salary && job.currency === 'USD' && <>$ {Number(job.Salary)/1000}k</>}
-                {job.Salary && job.currency === 'INR' && <>₹ {Number(job.Salary)/1000}k</>}
+                {job.Salary && job.currency === 'USD' && <>$ {Number(job.Salary) / 1000}k</>}
+                {job.Salary && job.currency === 'INR' && <>₹ {Number(job.Salary) / 1000}k</>}
               </span>
               <span className="px-2.5 py-1 bg-zinc-800 text-zinc-300 rounded-full text-xs">
                 {/* {job.minExperience}-{job.maxExperience} Yrs */}
@@ -147,30 +165,31 @@ export default function Jobs() {
           </Card>
         ))}
       </div>
-      { jobDetailView && 
-        <div className="inset-0 absolute w-full h-full bg-zinc-700/70 backdrop-blur-xs">
+      {jobDetailView &&
+        <div className="inset-0 fixed z-10 w-full h-screen bg-zinc-700/70 backdrop-blur-xs">
           <AnimatePresence mode="wait">
-            <motion.div 
+            <motion.div
               key="job-view-modal"
               initial={{
                 opacity: 0.7,
                 translateY: 50,
               }}
               animate={{
-                  opacity: 1,
-                  translateY: 0,
+                opacity: 1,
+                translateY: 0,
               }}
               exit={{
                 opacity: 0.6,
                 translateY: 50
               }}
               transition={{
-                  duration: 0.2,
-                  ease: "easeInOut"
-              }} 
-              ref={viewRef} 
-              className="absolute bottom-0 w-full h-[calc(100%-16rem)]"
+                duration: 0.2,
+                ease: "easeInOut"
+              }}
+              ref={viewRef}
+              className="absolute bottom-[0rem] w-full  h-[calc(100%-12rem)] "
             >
+              {/**/}
               <JobView jobId={selectedJobId} setDetailView={setJobDetailView} />
             </motion.div>
           </AnimatePresence>
--- src/components/job-view.tsx
+++ src/components/job-view.tsx
@@ -89,16 +89,16 @@ export default function JobView({ jobId, setDetailView }: JobViewProps) {
         fetchJobDetails();
     }, [jobId, toast]);
 
-    async function applyToJob(){
-        try {            
+    async function applyToJob() {
+        try {
             const appliedRes: ApplyJobResponse = await recordApplyJob(jobId, letter);
             // console.log("appliedRes", appliedRes);
-            if(!appliedRes.status){
+            if (!appliedRes.status) {
                 toast({
                     variant: "destructive",
                     title: appliedRes?.error || "Your application couldn't be submitted, Please try again later.",
                     description: appliedRes?.message
-                }); 
+                });
                 return;
             }
             toast({
@@ -113,8 +113,15 @@ export default function JobView({ jobId, setDetailView }: JobViewProps) {
 
     if (loading) {
         return (
-            <section className="w-full h-full min-h-96 bg-zinc-900 rounded-t-xl py-3 px-5">
-                <div className="animate-pulse">Loading...</div>
+            <section className="w-full h-full min-h-96 flex gap-5 justify-center items-center bg-zinc-900 rounded-t-xl py-3 px-5">
+                <div role="status">
+                    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                </div>
+                <div className="text-3xl text-white font-semibold">Loading....</div>
             </section>
         );
     }
@@ -128,7 +135,7 @@ export default function JobView({ jobId, setDetailView }: JobViewProps) {
     }
 
     return (
-        <section className="w-full h-full min-h-96 bg-zinc-900 rounded-t-xl py-3 px-5 overflow-y-auto
+        <section className="w-full h-full min-h-96 bg-zinc-900 rounded-t-xl py-3 px-5 max-h-[85vh] overflow-y-auto
             [&::-webkit-scrollbar]:w-2
             [&::-webkit-scrollbar-track]:bg-zinc-800
             [&::-webkit-scrollbar-thumb]:bg-zinc-600
@@ -137,22 +144,22 @@ export default function JobView({ jobId, setDetailView }: JobViewProps) {
             [&::-webkit-scrollbar-thumb]:border-zinc-800
             [&::-webkit-scrollbar-thumb]:hover:bg-zinc-500"
         >
-            <button 
-                className="absolute top-4 right-7 p-1 rounded-full hover:bg-zinc-800" 
+            <button
+                className="absolute top-4 right-7 p-1 rounded-full hover:bg-zinc-800"
                 onClick={() => setDetailView(false)}
             >
                 <Image
-                  src='/images/Clear.svg'
-                  width={18}
-                  height={18}
-                  alt="bookmark"
+                    src='/images/Clear.svg'
+                    width={18}
+                    height={18}
+                    alt="bookmark"
                 />
             </button>
-            
+
             <div className="max-w-4xl mx-auto pt-8">
                 <div className="flex items-start gap-4">
                     <Image
-                        src={job.orgLogo?? ''}
+                        src={job.orgLogo ?? ''}
                         alt={job.orgName}
                         width={80}
                         height={80}
@@ -176,8 +183,8 @@ export default function JobView({ jobId, setDetailView }: JobViewProps) {
                     <div className="bg-zinc-800/50 p-4 rounded-lg">
                         <p className="text-zinc-400">Salary</p>
                         <p className="text-white">
-                            {job.currency === 'USD' && `$ ${Number(job.Salary)/1000}k`}
-                            {job.currency === 'INR' && `₹ ${Number(job.Salary)/1000}k`}
+                            {job.currency === 'USD' && `$ ${Number(job.Salary) / 1000}k`}
+                            {job.currency === 'INR' && `₹ ${Number(job.Salary) / 1000}k`}
                         </p>
                     </div>
                 </div>
@@ -186,7 +193,7 @@ export default function JobView({ jobId, setDetailView }: JobViewProps) {
                     <h2 className="text-xl font-semibold text-white mb-4">Required Skills</h2>
                     <div className="flex flex-wrap gap-2">
                         {job.requiredSkills.map((skill) => (
-                            <span 
+                            <span
                                 key={skill}
                                 className="px-3 py-1 bg-purple-500/10 text-purple-400 rounded-full text-sm"
                             >
@@ -203,17 +210,17 @@ export default function JobView({ jobId, setDetailView }: JobViewProps) {
 
                 <div className="mt-8">
                     <h2 className="text-xl font-semibold text-white mb-4">Want to apply ? Write a cover letter</h2>
-                    <textarea 
-                        name="" id="" 
-                        placeholder="Write your cover letter" 
+                    <textarea
+                        name="" id=""
+                        placeholder="Write your cover letter"
                         className="w-1/2 rounded-xl focus:outline-hidden py-2 px-4 text-white"
                         onChange={(e) => setLetter(e.target.value)}
                     ></textarea>
                 </div>
-                
+
                 <div className="mt-8">
-                    <Button 
-                        disabled={letter.length < 1} 
+                    <Button
+                        disabled={letter.length < 1}
                         variant="default" className="text-black bg-white hover:bg-zinc-300 w-1/2"
                         onClick={applyToJob}
                     >
